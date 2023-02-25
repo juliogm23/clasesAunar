0eb9d2d456311865a09f30c133bdf4881e35117c
David Caro <dacamo0502@gmail.com>
2023-02-25 14:21:53 -0500

    Modificaciones actuales al trabajo de yorleidys.

--- src/app.js
+++ src/app.js
@@ -1,17 +1,8 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const routes = require('./routes/indexRouter')
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.send('App is working'))
-
-app.use('/clasesAunar/src/routes', indexRouter)
-
-app.listen(3000, () => console.log('servidor corriendo 3000!'))
-
-module.exports = {
-  app
-}
+app.listen(3000, () => console.log("servidor corriendo 3000!"));
