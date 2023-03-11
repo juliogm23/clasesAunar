8caf75777727620882bde6bdb31e24a16d7edf9e
shelly <shellyriios@gmail.com>
2023-03-11 16:46:52 -0500

    guardando estudiantes

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -9,4 +9,16 @@ controller.index = async (req, res) => {
     res.send(d)
 }
 
+controller.guardarEstudiante = async (req, res) => {
+    await estudiantes.create({
+        nombre: 'alice123', 
+        apellido: 'martinez', 
+        tip_documento: 'cedula de ciudadania',
+        num_documento: '12345678899', 
+        correo: 'alicemartinez@gmail.com', 
+        carrera: 'contaduria',
+      }, { fields: ['nombre','apellido','tip_documento','num_documento','correo','carrera'] });
+}
+
+  
 module.exports = controller
